de30772a1ce9ab97ec33c9e127349637e67364fd
resourcefulmind <61961225+resourcefulmind@users.noreply.github.com>
2020-11-26 11:35:27 +0100

    --- Auto-Git Commit ---

---
+++ calculator.js
@@ -0,0 +1,22 @@
+// we need to have something that holds a previous number when we do an operation with a new number, say a running total something like where we start off
+let runningTotal = 0;
+
+// Next we should have sort of like a buffer, what is on the screen at any given time
+let buffer = "0";
+
+// then we need to keep the previous operation in another store, sort of if we add before and we want to subtract now
+let previousOperator;
+
+// next we grab the screen 
+const screen = document.querySelector('.screen');
+
+//starting with what happens when a user clicks one of the buttons
+function buttonClick() {}
+
+// the init function will be the function that gets called once and sets everything up
+function init () {
+
+}
+
+// then call init
+init();
